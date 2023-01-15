13756b4a7b078e4098f2b0cf95b2402ffe24faa8
s18345 <wszczepanski97@gmail.com>
2023-01-15 22:48:01 +0100

    #52 Repair workers dashboards

--- templates/mechanic/dashboard/components/servicesgrid/components/servicesgridlayout/servicesgridlayout.component.tsx
+++ templates/mechanic/dashboard/components/servicesgrid/components/servicesgridlayout/servicesgridlayout.component.tsx
@@ -8,55 +8,60 @@ const ServicesGridLayout: FC = ({ children }) => {
   const {
     screen: { active },
   } = useContext(FullScreenContext);
+  const layoutProperties = [
+    {
+      i: "current-services",
+      x: 0,
+      y: 0,
+      w: 1,
+      h: active ? 4 : 3,
+      isResizable: false,
+    },
+    {
+      i: "future-services",
+      x: 1,
+      y: 0,
+      w: 2,
+      h: 3.5,
+      isResizable: false,
+    },
+    {
+      i: "available-services",
+      x: 1,
+      y: 1,
+      w: 2,
+      h: 3.5,
+      isResizable: false,
+    },
+    {
+      i: "past-services",
+      x: 3,
+      y: 0,
+      w: 1,
+      h: active ? 8 : 7,
+      isResizable: false,
+    },
+    {
+      i: "paychecks",
+      x: 0,
+      y: 1,
+      w: 1,
+      h: 4,
+      isResizable: false,
+    },
+  ];
   const layout = {
-    lg: [
-      {
-        i: "current-services",
-        x: 0,
-        y: 0,
-        w: 1,
-        h: active ? 4 : 3,
-        isResizable: false,
-      },
-      {
-        i: "future-services",
-        x: 1,
-        y: 0,
-        w: 2,
-        h: 3.5,
-        isResizable: false,
-      },
-      {
-        i: "available-services",
-        x: 1,
-        y: 1,
-        w: 2,
-        h: 3.5,
-        isResizable: false,
-      },
-      {
-        i: "past-services",
-        x: 3,
-        y: 0,
-        w: 1,
-        h: active ? 8 : 7,
-        isResizable: false,
-      },
-      {
-        i: "paychecks",
-        x: 0,
-        y: 1,
-        w: 1,
-        h: 4,
-        isResizable: false,
-      },
-    ],
+    lg: layoutProperties,
+    md: layoutProperties,
+    sm: layoutProperties,
+    xs: layoutProperties,
+    xxs: layoutProperties,
   };
   return (
     <ResponsiveGridLayout
       layouts={layout}
       breakpoints={{ lg: 1200, md: 996, sm: 768, xs: 480, xxs: 0 }}
-      cols={{ lg: 4 }}
+      cols={{ lg: 4, md: 4, sm: 4, xs: 4, xxs: 4 }}
       rowHeight={80}
       width={active ? 1920 : 1200}
     >
